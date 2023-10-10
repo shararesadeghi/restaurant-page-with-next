0f84f75827da4ab824a385e8ede0a7b873f672b8
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-10 17:18:17 +0330

    Attributes in homepage completed

--- project/components/modules/Attributes.js
+++ project/components/modules/Attributes.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Choice from '../icons/Choice';
+import Clock  from '../icons/Choice';
+import Fast from "../icons/Fast";
+import Food from "../icons/Food";
+import styles from "./Attributes.module.css";
+
 
 const Attributes = () => {
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <h3>Why us?</h3>
+            <div className={styles.subContainer}>
+                <div>
+                    <Fast/>
+                    <p>Fast</p>
+                </div>
+                <div>
+                    <Food/>
+                    <p>Best Restaurants</p>
+                </div>
+                <div>
+                    <Choice/>
+                    <p>Your Choice</p>
+                </div>
+                <div>
+                    <Clock/>
+                    <p>24-7</p>
+                </div>
+            </div>
         </div>
     );
 };
