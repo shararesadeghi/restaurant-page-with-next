0c14a28273f30a58f657bc6fa1125ad77a26ddf7
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-10 16:36:54 +0330

    layout for header & footer completed

---
+++ project/components/layout/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './Layout.module.css';
+
+const Layout = ({children}) => {
+    return (
+        <>
+         <header className={styles.header}>
+             <div className={styles.left}>
+                 <Link href="/">Restaurant</Link>
+             </div>
+             <div className={styles.right}>
+                 <Link href="/menu">Menu</Link>
+                 <Link href="/categories">Categories</Link>
+             </div>
+        </header> 
+        <div className={styles.container}>{children}</div>  
+        <footer className={styles.footer}>
+            Next.js course | Restaurant Project &copy;
+        </footer>
+        </>
+    );
+};
+
+export default Layout;
