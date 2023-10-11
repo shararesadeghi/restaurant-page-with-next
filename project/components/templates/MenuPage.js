ccbe6cddc9fbdadd516fd65177a4b9b9ce226bc1
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-11 15:42:13 +0330

    completdemenu route& card component

--- project/components/templates/MenuPage.js
+++ project/components/templates/MenuPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Card from '../modules/Card';
 import styles from './MenuPage.module.css';
 
 const MenuPage = ({data}) => {
     return (
-        <div>
-            MenuPage
+        <div className={styles.container}>
+            <h2>Menu</h2>
+            <div className={styles.subContainer}>
+                {data.map(food=>(
+                    <Card key={food.id} {...food}/>
+                ))}
+            </div>
         </div>
     );
 };
