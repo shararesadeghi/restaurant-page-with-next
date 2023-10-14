5a30cc50d6891db61d89268bb06e81b744d0f9fe
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-14 15:50:58 +0330

    add queries to categories page

--- project/components/templates/CategoriesPage.js
+++ project/components/templates/CategoriesPage.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {useRouter} from 'next/router';
 import styles from './CategoriesPage.module.css';
 
 const CategoriesPage = () => {
+    const router = useRouter();
+
+    const [query, setQuery] = useState({difficulty:"", time:""});
+
+    const changeHandler = event=>{
+        setQuery({...query,[event.target.name]:event.target.value})
+    }
+
+    const searchHandler = ()=>{
+        router.push({pathname:"/categories", query})
+    }
     return (
         <div className={styles.container}>
            <h2>Categories</h2>
            <div className={styles.subContainer}>
                <div className={styles.select}>
-                   <select>
-                       <option>Difficulty</option>
-                       <option>Easy</option>
-                       <option>Medium</option>
-                       <option>Hard</option>
+                   <select value={query.difficulty} name="difficulty" onChange={changeHandler}>
+                       <option vlaue="">Difficulty</option>
+                       <option vlaue="Esay">Easy</option>
+                       <option vlaue="Medium">Medium</option>
+                       <option vlaue="Hard">Hard</option>
                    </select>
-                   <select>
-                       <option>Cooking Time</option>
-                       <option>More than 30 min </option>
-                       <option>Less than 30 min</option>
+                   <select value={query.time} name="time" onChange={changeHandler}>
+                       <option value="">Cooking Time</option>
+                       <option vlau="more">More than 30 min </option>
+                       <option vlaue="less">Less than 30 min</option>
                    </select>
-                   <button>Search</button>
+                   <button onClick={searchHandler}>Search</button>
                </div>
            </div>
         </div>
