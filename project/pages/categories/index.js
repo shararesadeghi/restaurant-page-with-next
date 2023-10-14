d8211680ce90fceb76e99933bc0dad72673cea56
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-14 15:23:34 +0330

    create ui forcategoriespage

---
+++ project/pages/categories/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CategoriesPage from '../../components/templates/CategoriesPage';
+
+const Categories = () => {
+    return (
+        <CategoriesPage/>
+    );
+};
+
+export default Categories;
